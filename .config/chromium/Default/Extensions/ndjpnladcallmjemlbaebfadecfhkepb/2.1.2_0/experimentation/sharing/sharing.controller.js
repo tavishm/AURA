abbc8f0f7baffb251474c183bec5664c9d54803a
Tavish Mankash <tavish.mankash@gmail.com>
2025-03-02 18:02:02 +0530

    Delete .config directory

--- .config/chromium/Default/Extensions/ndjpnladcallmjemlbaebfadecfhkepb/2.1.2_0/experimentation/sharing/sharing.controller.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";angular.module("app.experimentation").controller("SharingController",["experimentService",function(n){var t=this,i="/experimentation/sharing/",e="SHARING";t.flightView=i+"sharingControl/sharingControl.html",n.getFlight(e).then((function(n){t.flightView=i+n}))}])}();
